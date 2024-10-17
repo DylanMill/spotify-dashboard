47ba881f207cb85521aa5001ec7250ae26ee54d0
DylanMill <Dylanmill@outlook.com>
2024-10-17 02:16:00 -0300

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import AuthorizeButton from "@/components/AuthorizeButton";
+
+export default function Home() {
+   return (
+      <main className="flex min-h-screen items-center justify-center">
+         <AuthorizeButton></AuthorizeButton>
+      </main>
+   );
+}
---
+++ src/components/AuthorizeButton.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const AuthorizeButton = () => {
+   const scopes: string[] = [
+      "ugc-image-upload",
+      "user-read-playback-state",
+      "user-modify-playback-state",
+      "user-read-currently-playing",
+      "app-remote-control",
+      "streaming",
+      "playlist-read-private",
+      "playlist-read-collaborative",
+      "playlist-modify-private",
+      "playlist-modify-public",
+      "user-follow-modify",
+      "user-follow-read",
+      "user-read-playback-position",
+      "user-top-read",
+      "user-read-recently-played",
+      "user-library-modify",
+      "user-library-read",
+      "user-read-email",
+      "user-read-private",
+   ];
+
+   const AUTH_URL = "https://accounts.spotify.com/authorize";
+
+   const url = `${AUTH_URL}?client_id=${
+      process.env.NEXT_PUBLIC_CLIENT_ID
+   }&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&scope=${scopes.join(
+      " "
+   )}`;
+
+   return (
+      <>
+         <Button className="auth-button">
+            <Link href={url}>Sign in to Spotify</Link>
+         </Button>
+      </>
+   );
+};
+
+export default AuthorizeButton;
